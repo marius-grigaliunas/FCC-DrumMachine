aea74a9c597a1e2c52de197c5ffcbe87e49758cd
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-02-18 21:10:52 +0100

    recording play button is disabled during playing

--- src/components/RecordedField.tsx
+++ src/components/RecordedField.tsx
@@ -1,17 +1,42 @@
+import { useRef, useState } from "react";
 
 interface RecordedFieldProps {
     recordings: { letter: string; sound: string; time: number; sourceLink: string}[][];
 }
 export const RecordedField = ({ recordings }: RecordedFieldProps) => {
-    const playRecording = (recording: { letter: string; sound: string; time: number; sourceLink: string }[]) => {
-        recording.forEach(({ sound, time, sourceLink }) => {
-            setTimeout(() => {
-                console.log(`Playing: ${sound} at ${time}ms`);
-                const audio = new Audio(sourceLink);
-                audio.play().catch(err => console.error("Audio play error:", err));
-            }, time);
-        });
-    };
+    const playButtonRef = useRef<HTMLButtonElement | null>(null)
+    const [isPlaying, setIsPlaying] = useState(false)
+
+    const playRecording = async (recording: { letter: string; sound: string; time: number; sourceLink: string }[]) => {
+        console.log("Record started")
+    
+        if (playButtonRef.current) {
+            playButtonRef.current.classList.add("disabled")
+        }
+        setIsPlaying(true)
+
+        const playPromises = recording.map(({ time, sourceLink }) => {
+            return new Promise<void>((resolve) => {
+                setTimeout(() => {
+    
+                    const audio = new Audio(sourceLink);
+                    audio.play().then(() => {
+                        resolve()
+                    }).catch(err => {
+                        console.error("Audio play error:", err)
+                        resolve()
+                    })
+                }, time)
+            })
+        })
+    
+        await Promise.all(playPromises);
+    
+        if (playButtonRef.current) {
+            playButtonRef.current.classList.remove("disabled");
+        }
+        setIsPlaying(false)
+    }
 
     return (
         <div id='record-field-container' className='bg-zinc-900 border border-pink-950 h-96 rounded-2xl mt-3 p-3 overflow-y-auto'>
@@ -28,10 +53,13 @@ export const RecordedField = ({ recordings }: RecordedFieldProps) => {
                             </p>
                         ))}
                         <button
-                            className="mt-2 px-4 py-1 bg-pink-600 text-white rounded"
+                            ref={playButtonRef}
+                            className="mt-2 px-4 py-1 bg-pink-900 border-2 border-pink-950 ring ring-pink-900 text-white 
+                            rounded-2xl"
                             onClick={() => playRecording(recording)}
+                            disabled={isPlaying ? true : false}
                         >
-                            ▶ Play
+                            Play
                         </button>
                     </div>
                 ))
